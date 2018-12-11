6d6a4f226256e329192f2b0af6730ffc4aae1b82
Rodolfo Robles <rjroblesq@gmail.com>
2018-12-11 16:54:20 -0600

    lol

--- Items/Apple.cs
+++ Items/Apple.cs
@@ -12,20 +12,9 @@ namespace SucculentMod.Items
 		}
 		public override void SetDefaults()
 		{ 
-            
             item.CloneDefaults(ItemID.CandyApple);
-		    item.maxStack = 50;
-		    item.reuseDelay = 5;
-		    item.healLife = 100;
-		}
-
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+		    item.maxStack = 200;
+		    item.healLife = 5;
 		}
 	}
 }
